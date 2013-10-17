6571cba38b7248f43fe68c6e3089b7df65d97f1a
unknown <jbell101@gmail.com>
2013-10-17 13:52:13 -0700

    First working Windows Application (not very OO).

--- Game Tutorial/main.cpp
+++ Game Tutorial/main.cpp
@@ -1,47 +1,14 @@
-#define WIN32_LEAN_AND_MEAN
+//#define WIN32_LEAN_AND_MEAN
 
 #include <Windows.h>
-
-LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
-
-void windowClassInit(WNDCLASSEX *windowClass, HINSTANCE hInstance)
-{
-	windowClass->cbSize = sizeof(WNDCLASSEX);
-	windowClass->style = CS_VREDRAW;
-	windowClass->lpfnWndProc = WndProc;
-	windowClass->cbClsExtra = 0;
-	windowClass->cbWndExtra = 0;
-	windowClass->hInstance = hInstance;
-	windowClass->hIcon = LoadIcon(hInstance, IDI_APPLICATION);
-	windowClass->hCursor = LoadCursor(hInstance, IDC_ARROW);
-	windowClass->hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-	windowClass->lpszMenuName = NULL;
-	windowClass->lpszClassName = L"First Windows Program";
-	windowClass->hIconSm = LoadIcon(NULL, IDI_WINLOGO);	
-}
-
-void windowClassCreate(HWND *hwnd, HINSTANCE hInstance)
-{
-	*hwnd = CreateWindowEx(NULL,
-						 L"MyClass",
-						 L"A REAL Windows Application!",
-						 WS_OVERLAPPEDWINDOW | 
-						 WS_VISIBLE |
-						 WS_SYSMENU,
-						 100, 100,
-						 400, 400,
-						 NULL,
-						 NULL,
-						 hInstance,
-						 NULL);
-}
+#include <tchar.h>
 
 /* Use the Windows Procedure event handler */
 LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	PAINTSTRUCT paintStruct;
 	HDC hDC;
-	const wchar_t *string = L"Hello World";
+	static TCHAR string[] = _T("Hello World");
 
 	switch (message) {
 		case WM_CREATE:		/* Window is being created */
@@ -49,6 +16,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 		case WM_CLOSE:
 			PostQuitMessage(0);
 			return 0;
+			break;
 		case WM_PAINT:		
 			/* What happens when the window is moved, resized, 
 			 * or any other event occurs 
@@ -56,10 +24,10 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 			hDC = BeginPaint(hwnd, &paintStruct);
 
 			/* Set text color to green */
-			SetTextColor(hDC, COLORREF(0x0000ff00));
+			SetTextColor(hDC, COLORREF(0x00006600));
 
 			/* Display the text in the middle of the window */
-			TextOut(hDC, 150, 150, string, sizeof(string) - 1);
+			TextOut(hDC, 150, 150, string, wcslen(string));
 
 			EndPaint(hwnd, &paintStruct);
 			return 0;
@@ -73,22 +41,52 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 /* Main application entry point */
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
-	WNDCLASSEX	windowClass;		/* Window Class */
+	static TCHAR szWindowClass[] = _T("...");
+	static TCHAR szTitle[] = _T("Title");
+
+	WNDCLASSEX	wcex;				/* Window Class */
 	HWND		hwnd;				/* Window Handle */
 	MSG			msg;				/* message */
 	bool		done;				/* Flow saying when your app is complete */
 
 	/* Fill out the Window Class struct */
-	windowClassInit(&windowClass, hInstance);
+	//windowClassInit(&windowClass, hInstance);
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, IDI_QUESTION);
+    wcex.hCursor        = LoadCursor(NULL, IDC_CROSS);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = NULL;
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, IDI_QUESTION);
 
 	/* Register the Windows Class */
-	if (!RegisterClassEx(&windowClass))
-		return 0;
+	if (!RegisterClassEx(&wcex))
+		return GetLastError();
 
 	/* Class registered, now create the window */
-	windowClassCreate(&hwnd, hInstance);
-	if (!(hwnd  = NULL))
-		return 0;
+	//windowClassCreate(&hwnd, hInstance);
+
+	hwnd = CreateWindowEx(NULL,
+						 szWindowClass,
+						 szTitle,
+						 WS_OVERLAPPEDWINDOW | 
+						 WS_VISIBLE,
+						 //WS_SYSMENU,
+						 100, 100,
+						 400, 400,
+						 NULL,
+						 NULL,
+						 hInstance,
+						 NULL);
+	
+	if (!hwnd) {
+		return GetLastError();
+	}
 
 	done = false;
 
